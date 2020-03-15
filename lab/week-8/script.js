844ea13ef879752aa87772b7d889dfe8642fd3a5
andrew_l <andrewlevins@gmail.com>
2020-03-15 13:10:37 -0400

    clean up and add code comments

--- lab/week-8/script.js
+++ lab/week-8/script.js
@@ -1,54 +1,77 @@
 // terminology check
-// [] is a square bracket used in an array
-// {} those are curly braces used in an object and function
-// () those are parentheses used in many different places like functions,querySelector(), etc.
+// [] is a 'square bracket' used in an array
+// {} those are 'curly braces' used in an object and function
+// () those are 'parentheses' used in many different places like functions,querySelector(), etc.
 
 // this is the OLD way we created an array. By only putting a single string in each index (between the commas,).
-// This is okay at the beginning, but it only contains a single piece of information
-// var archiveItems = ['image-1.jpg', 'image-2.jpg', 'image-3.jpg'];
+var oldItemsArrayExample = ['image-1.jpg', 'image-2.jpg', 'image-3.jpg', '...'];
+// This is okay to use in the beginning, but it only contains a single piece of information. It won't work when you want more information for each item...
 
+// INSTEAD
 // here, we use {Objects} in each index to hold multiple pieces of information for each archive item.
+// note the { property: 'value' } syntax similar to CSS, except here you get to create the property name too
 var organizedArchiveItems = [
   {
-    title: '1 — Talented',
+    title: 'Talented',
     imageURL: 'talented.jpg',
-    description: 'a description',
-    link: 'a url to the source',
+    description: '1 — a description',
+    credit: 'Photo by Jamie Street on Unsplash',
     year: 1984
   },
   {
-    title: '2 — Brilliant',
+    title: 'Brilliant',
     imageURL: 'brilliant.jpg',
-    description: 'a description of the second',
-    link: 'a url to the source of the second',
+    description: '2 — a description of the second',
+    credit: 'Photo by Anoir Chafik on Unsplash',
     year: 2003
   },
   {
-    title: '3 — Incredible',
+    title: 'Incredible',
     imageURL: 'incredible.jpg',
-    description: 'a description of the third',
-    link: 'a url to the source of the third',
+    description: '3 — a description of the third',
+    credit: 'Photo by Martin Moreno on Unsplash',
     year: 1980
   },
   {
-    title: '4 — Amazing',
+    title: 'Amazing',
     imageURL: 'amazing.jpg',
-    description: 'a description of the fourth',
-    link: 'a url to the source of the fourth',
+    description: '4 — a description of the fourth',
+    credit: 'Photo by Julian Schiemann on Unsplash',
     year: 1830
   },
   {
-    title: '5 — Show-Stopping',
+    title: 'Show-Stopping',
     imageURL: 'show-stopping.jpg',
-    description: 'a description of the fifth',
-    link: 'a url to the source of the fifth',
+    description: '5 — a description of the fifth',
+    credit: 'Photo by Jamie Street on Unsplash',
     year: 2013
   },
   {
-    title: '6 — Spectacular',
+    title: 'Spectacular',
     imageURL: 'spectacular.jpg',
-    description: 'a description of the sixth',
-    link: 'a url to the source of the sixth',
+    description: '6 — a description of the sixth',
+    credit: 'Photo by Sneaky Elbow on Unsplash',
+    year: 1999
+  },
+  {
+    title: 'Never the Same',
+    imageURL: 'never-the-same.jpg',
+    description: '7 — a description of the seventh',
+    credit: 'Photo by Sara Kurfeß on Unsplash',
+    year: 2004
+  },
+  {
+    title: 'Totally Unique',
+    imageURL: 'totally-unique.jpg',
+    description: '8 — a description of the eighth',
+    credit: 'Photo by Vitor Fontes on Unsplash',
+    year: 2014
+  },
+  {
+    title: 'Completely Not Ever Been Done Before',
+    imageURL: 'completely-not-ever-been-done-before.jpg',
+    description: '9 — a description of the ninth',
+    credit: 'Photo by NICOLAS TESSARI on Unsplash',
     year: 1999
   }
 ];
@@ -57,9 +80,9 @@ var organizedArchiveItems = [
 var grid = document.querySelector('.grid');
 
 // main function that generates our content when the page loads, when we click the button, or when we filter
-function generate(filteredItems) {
+function generate(items) {
   grid.innerHTML = ''; // clear the existing items first
-  filteredItems.forEach(item => {
+  items.forEach(item => {
     // we use += inside a loop to ADD every item, one after another.
     grid.innerHTML += `
   <div class="item">
@@ -67,15 +90,15 @@ function generate(filteredItems) {
     <img src="images/${item.imageURL}" class="archive-image"/>
     <h5>${item.title}</h5>
     <p class="description">${item.description}</p>
-    <a class="source">${item.link}</a>
-</div>
+    <p class="source">${item.credit}</p>
+  </div>
     `;
   });
 }
-generate(organizedArchiveItems); // run function on page load with our full array of items
+generate(organizedArchiveItems); // run function on page load, passing in our full array of items
 
 function filterContent() {
-  var value = +event.target.value; // get the value from the HTML select dropdown
+  var value = +event.target.value; // get the value from the HTML select dropdown and convert to number with "+"
   var filteredItems = organizedArchiveItems.filter(item => {
     return item.year < value; // loops through all items in array and filters out items that don't have a year less than the dropdown value
   });
@@ -84,8 +107,9 @@ function filterContent() {
 
 function randomize() {
   // array shuffle code credit: https://css-tricks.com/snippets/javascript/shuffle-array/
-  var randomItems = organizedArchiveItems.sort(() => {
-    return 0.5 - Math.random(); // don't worry about why this works, it's complicated. We are creating a new array but the {objects} are in a different order (like shuffling a deck of cards)
-  });
+  // don't worry about why this works, it's complicated. We are creating a new array based on the original organizedArchiveItems
+  // but the { objects } are in a different order...like shuffling a deck of cards
+  var randomItems = organizedArchiveItems.sort(() => 0.5 - Math.random());
+
   generate(randomItems); // run function to generate content with new randomItems array
 }
