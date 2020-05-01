74fc8f7918e45523826f203c7e1f66523fcb4be6
andrew_l <andrewlevins@gmail.com>
2020-05-01 01:08:38 -0400

    lesson 14 api example

--- lab/week-14/in-class-example/script.js
+++ lab/week-14/in-class-example/script.js
@@ -1,16 +1,14 @@
 fetch('https://api.themoviedb.org/3/trending/all/week?api_key=94a83d421d79431debf81630227da2ff')
   .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    buildContent(data);
-  });
+  .then(data => buildContent(data));
 
 const BASE_URL = 'http://image.tmdb.org/t/p/w500';
-const content = document.querySelector('.content');
+const content = document.querySelector('.grid');
 var movieSet = [];
 
 function buildContent({ results }) {
-  const moviesContent = results
+  content.innerHTML = results
+    .filter(item => item.media_type === 'movie')
     .map(movie => {
       movieSet[movie.id] = movie;
       return `
@@ -21,28 +19,35 @@ function buildContent({ results }) {
     })
     .join('');
 
-  content.innerHTML = `
-            <ul class="grid">
-        ${moviesContent}
-    </ul>`;
-
   document.querySelectorAll('.movie').forEach(movie => movie.addEventListener('click', showDetails));
 }
 
 const details = document.querySelector('.details');
-async function showDetails() {
-  if (!event.target.dataset.id) return;
 
+async function showDetails() {
   details.style.display = 'block';
 
   const movieID = event.target.dataset.id;
 
-  console.log(movieSet[movieID]);
-  const { title, name, overview, media_type, vote_average, backdrop_path } = movieSet[movieID];
+  // console.log(movieSet[movieID]);
+  const { title, name, overview, media_type, vote_average, backdrop_path, poster_path } = movieSet[movieID];
 
   const videoInfo = await fetch(`https://api.themoviedb.org/3/movie/${movieID}/videos?api_key=94a83d421d79431debf81630227da2ff&language=en-US`).then(res => res.json());
-  console.log(videoInfo);
   const video_id = videoInfo.results[0] ? videoInfo.results[0].key : null;
+  console.log(videoInfo);
+
+  const recos = await fetch(`https://api.themoviedb.org/3/movie/${movieID}/recommendations?api_key=94a83d421d79431debf81630227da2ff&language=en-US&page=1`).then(res => res.json());
+  const recosList = recos.results
+    .slice(0, 3)
+    .map(x => {
+      return ` 
+      <div>
+        <h4 class="rec-title">${x.title}</h4>
+        <img src="http://image.tmdb.org/t/p/w500${x.poster_path}" />
+      </div>
+    `;
+    })
+    .join('');
 
   details.innerHTML = `
     <div id="detail-content">
@@ -51,18 +56,19 @@ async function showDetails() {
             <div class="tag">${media_type}</div>
             <div class="tag">${vote_average} ☆</div>
         </div>
-        <h3 class="title">${title ? title : name}</h3>
+        <h3 class="title">${title}</h3>
         <p>${overview}</p>
-        <div class="related">
+        <a class="trailer" href="https://www.youtube.com/watch?v=${video_id}" target="_blank">Watch Trailer ↗</a>
+      <div class="related">
+        <h4>Recommendations</h4>
+        <div class="flex">
+          ${recosList}
         </div>
-        <iframe class="bkg-img"  src="https://www.youtube.com/embed/${video_id}?autoplay=1&showinfo=0&controls=0&mute=1&amp;start=5" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;"></iframe>        </div>
-        `;
+      </div>
+      <img src="http://image.tmdb.org/t/p/w1280${backdrop_path}" class="bkg-img" />
+      </div>
+      `;
 
-  // <img src="http://image.tmdb.org/t/p/w1280${backdrop_path}" class="bkg-img" />
-  details.addEventListener('click', () => {
-    if (event.target.children.length && event.target.id !== 'detail-content') {
-      details.style.display = 'none';
-    }
-  });
+  // <iframe class="bkg-img"  src="https://www.youtube.com/embed/${video_id}?autoplay=1&showinfo=0&controls=0&mute=1&amp;start=5" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;"></iframe>
   document.querySelector('.icon').addEventListener('click', () => (details.style.display = 'none'));
 }
